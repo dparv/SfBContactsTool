2657c23cb322afe369306421180bf62c8a7fba8e
PARVANOV Diko <parvanov@icc.unicc.org>
2018-08-30 09:19:14 +0200

    Add project files.

---
+++ Import/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Group;
+
+namespace WFP_Contacts
+{
+    class Program
+    {
+        public static LyncClient _client = LyncClient.GetClient();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking client state...");
+            CheckSignedIn(_client.State);
+            Console.WriteLine("Running import function...");
+            ImportContacts(_client);
+            string filePath = Directory.GetCurrentDirectory() + "BuddyList.xml";
+            Console.WriteLine("Import completed from file: " + filePath);
+        }
+        public static void ImportContacts(LyncClient _client)
+        {
+            ContactManager _contactManager = _client.ContactManager;
+            XmlDocument doc = new XmlDocument();
+            if (!File.Exists("BuddyList.xml"))
+            {
+                Console.WriteLine("BuddyList.xml does not exist.");
+                Environment.Exit(0);
+            }
+            doc.Load("BuddyList.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList groups = root.SelectNodes("ContactGroup");
+            GroupCollection current_groups = _contactManager.Groups;
+            var current_group_colelction = new List<string>();
+
+            foreach (Group group in current_groups)
+            {
+                current_group_colelction.Add(group.Name);
+            }
+
+            foreach (XmlNode group in groups)
+            {
+                String group_name = group.ChildNodes.Item(0).FirstChild.Value;
+                String group_type = group.Attributes.GetNamedItem("type").InnerText;
+                if (group_type == "DistributionGroup")
+                {
+                    _contactManager.BeginSearch(
+                        group_name,
+                        (ar) =>
+                    {
+                        SearchResults searchResults = _contactManager.EndSearch(ar);
+                        if (searchResults.Groups.Count > 0)
+                        {
+                            Console.WriteLine(searchResults.Groups.Count.ToString() + " found");
+                            foreach (Group dg in searchResults.Groups)
+                            {
+                                Console.WriteLine(System.Environment.NewLine + dg.Name);
+                                DistributionGroup dGroup = dg as DistributionGroup;
+                                if (!current_groups.Contains(dGroup))
+                                {
+                                    if (group_name == dGroup.Name) {
+                                        _contactManager.BeginAddGroup(dGroup, null, null);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No groups found for search on " + group_name);
+                        }
+                    }, 
+                    null);
+                }
+                else if (group_type == "FavoriteContacts" || group_type == "FrequentContacts")
+                {
+                    //skip Favourites as it always exists
+                    continue;
+                }
+                else
+                {
+                    //handle custom groups and favourites normally
+                    //check first if group exists or error
+                    if (!current_group_colelction.Contains(group_name))
+                    {
+                        //continue to adding new group
+                        _contactManager.BeginAddGroup(group_name, null, null);
+                        Console.WriteLine(group_name + " created.");
+                    }
+                    else
+                    {
+                        //show message and do not try to create group
+                        Console.WriteLine(group_name + " already exists.");
+                    }
+                }
+            }
+
+            //add the contacts to each group of typer Custom or Favourites
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Importing contacts...");
+            foreach (XmlNode group in groups)
+            {
+                if (group.Attributes.GetNamedItem("type").InnerText != "DistributionGroup")
+                {
+                    foreach (Group igroup in current_groups)
+                    {
+                        if (igroup.Name == group.ChildNodes.Item(0).FirstChild.Value)
+                        {
+                            Console.WriteLine(igroup.Name + " - " + igroup.Type.ToString());
+                            //add contacts
+                            foreach (XmlNode contact in group.ChildNodes.Item(1).ChildNodes)
+                            {
+                                Console.WriteLine(contact.InnerText);
+                                Contact c = _contactManager.GetContactByUri(contact.InnerText);
+                                if (!igroup.Contains(c)) {
+                                    igroup.BeginAddContact(c, null, null);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Finished import.");
+        }
+        public static void CheckSignedIn(ClientState state)
+        {
+            if (state != ClientState.SignedIn)
+            {
+                Console.WriteLine("Client not Signed In");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Client Found and Signed In - Continuing to Import");
+            }
+        }
+    }
+}
---
+++ WFPCExport/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Group;
+
+namespace WFP_Contacts
+{
+    class Program
+    {
+        public static LyncClient _client = LyncClient.GetClient();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking client state...");
+            System.Threading.Thread.Sleep(1000);
+            CheckSignedIn(_client.State);
+            Console.WriteLine("Running export function...");
+            ExportContacts(_client);
+            string filePath = Directory.GetCurrentDirectory() + "BuddyList.xml";
+            Console.WriteLine("Export completed in file: "+filePath);
+        }
+        public static void ExportContacts(LyncClient _client)
+        {
+            XmlTextWriter writer = new XmlTextWriter("BuddyList.xml", System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.WriteStartElement("LyncContacts");
+            foreach (Group group in _client.ContactManager.Groups)
+            {
+                //Do not export groups with 0 contacts in them
+                if (group.Count == 0)
+                {
+                    Console.WriteLine("Skipping empty group: "+group.Name);
+                    continue;
+                }
+                Console.WriteLine("Found group: '"+group.Name+"' of type '"+group.Type+"' with "+ group.Count+ " contacts");
+                writer.WriteStartElement("ContactGroup");
+                writer.WriteAttributeString("type", group.Type.ToString());
+                writer.WriteStartElement("Name");
+                writer.WriteCData(group.Name);
+                writer.WriteEndElement();
+                //Opening Contatacts XML element
+                writer.WriteStartElement("Contacts");
+                foreach (Contact contact in group)
+                {
+                    Console.WriteLine(contact.Uri);
+                    //Opening Single Contatact XML element
+                    writer.WriteStartElement("Contact");
+                    writer.WriteRaw(contact.Uri);
+                    writer.WriteEndElement();
+                    //Closing Single Contatact XML element
+                }
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                //Closing Contatacts XML element
+            }
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            writer.Close();
+        }
+        public static void CheckSignedIn(ClientState state)
+        {
+            if (state != ClientState.SignedIn)
+            {
+                Console.WriteLine("Client not Signed In");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Client Found and Signed In - Continuing to Export");
+            }
+        }
+    }
+}
